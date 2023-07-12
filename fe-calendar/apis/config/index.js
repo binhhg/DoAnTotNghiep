9c0bdc7af3c8bdbfae51b293aba739b54906e42b
binhhg <binhvxhg789@gmail.com>
2023-07-12 15:10:52 +0700

    chuyen fe

--- fe-calendar/apis/config/index.js
+++
@@ -1,8 +0,0 @@
-const user = process.env.USER_URL || 'http://localhost:8501'
-const calendar = process.env.CALENDAR_URL || 'http://localhost:8503'
-
-const ApiConfig = {
-    user,
-    calendar
-}
-export default ApiConfig
